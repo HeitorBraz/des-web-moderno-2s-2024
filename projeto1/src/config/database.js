05bb6c1a1c785f5678604aed5c46fd9bfaf8e593
Heitor Braz <heitorpitoco1109@gmail.com>
2024-09-17 10:35:14 -0300

    Projeto 1

--- projeto1/src/config/database.js
+++
@@ -1,9 +0,0 @@
-const { Sequelize } = require('sequelize');
-
-// Configura a conexão com o banco de dados SQLite
-const sequelize = new Sequelize({
-  dialect: 'sqlite',
-  storage: './src/database/database.sqlite'
-});
-
-module.exports = sequelize;
--- projeto1/src/routes/userRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const userController = require('../controllers/userController');
-
-router.get('/dashboard', userController.dashboard);
-
-module.exports = router;
